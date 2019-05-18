d151d054ebd9421b4d18edb472c91d7de0f14684
Quinten Steenhuis <qsteenhuis@gmail.com>
2019-05-18 13:56:51 +0000

    Don't try to call functions when iterating through fields in the uploaded form

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_package_data(where='.', package='', exclude=standard_exclude, exclude_d
     return out
 
 setup(name='docassemble.docsign',
-      version='0.8',
+      version='0.9',
       description=(''),
       long_description=u'# docassemble.docsign\r\n\r\n\r\n\r\n## Author\r\n\r\nQuinten Steenhuis, admin@admin.com\r\n\r\n',
       long_description_content_type='text/markdown',
